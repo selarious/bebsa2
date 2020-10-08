e16f1ec29e0e394bf1d77e252eb56c36b8eb6c58
Blukang25 <ndhoee@gmail.com>
2020-10-08 10:27:40 +0700

    menambahkan listing

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
 <body>
 <h1> Hello WOrld</h1>
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quis sed necessitatibus id in, cumque laudantium maxime? Mollitia incidunt quam iure temporibus aspernatur officia, animi eaque numquam ipsam totam provident!</p>
+<ul>
+<li> satu </li>
+<li> dua </li>
+</ul>
 </body>
 </html>
