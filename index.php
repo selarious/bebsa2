e559308118b4eb2c4a27e71de65740ee95c529cd
Blukang25 <ndhoee@gmail.com>
2020-10-08 10:19:55 +0700

    Memperbaharui file index.php

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 
 <body>
 <h1> Hello WOrld</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quis sed necessitatibus id in, cumque laudantium maxime? Mollitia incidunt quam iure temporibus aspernatur officia, animi eaque numquam ipsam totam provident!</p>
 </body>
 </html>
